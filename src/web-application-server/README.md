baa74932f700ac27c52819cb54a538dc750d05a0
apfp77 <apfp77@naver.com>
2024-03-01 09:10:35 +0900

    feat: chapter3 final

--- src/web-application-server/README.md
+++ src/web-application-server/README.md
@@ -14,17 +14,17 @@
 * 구현을 완료한 후 구현 과정에서 새롭게 알게된 내용, 궁금한 내용을 기록한다.
 * 각 요구사항을 구현하는 것이 중요한 것이 아니라 구현 과정을 통해 학습한 내용을 인식하는 것이 배움에 중요하다. 
 
+# 목차별 요구 사항
+## 3장
+<details>
+<summary> <b>요구사항</b> </summary>
+
 ### 요구사항 1 - http://localhost:8080/index.html로 접속시 응답
 <details>
 <summary><b>Hint</b></summary>
 
-### Hint 1
 * BufferedReader.readLine()을 사용하여 InputStream을 한 줄 단위로 읽을 수 있다
-
-### Hint 2
 * HTTP 요청 정보의 첫 번째 라인에서 요청 URL을 추출한다
-
-### Hint 3
 * 요청 URL에 해당하는 파일을 webapp 디렉토리에서 읽어 전달한다
 * Files.readAllBytes와 File("경로/파일명").toPath()을 사용한다
 
@@ -32,19 +32,49 @@
 </details>
 
 ### 요구사항 2 - get 방식으로 회원가입
-* 
+<details> <summary><b>Hint</b></summary>
+* HTTP 요청의 첫 번째 라인에서 요청 URL을 추출하여 User 클래스에 담아 DataBase 클래스에 저장한다
+* 이름=값 파싱은 util.HttpRequestUtils 클래스의 parseQueryString()메소드를 활용한다
+  * 요청 URL에서 "?"뒤의 문자열만 넣어야한다
+</details>
 
 ### 요구사항 3 - post 방식으로 회원가입
-* 
+<details> <summary><b>Hint</b></summary>
+* POST로 데이터를 전달할 경우 전달하는 데이터는 HTTP 본문에 담긴다
+* HTTP 본문은 HTTP 헤더 이후 빈 공백을 가지는 한 줄 다음부터 시작한다
+* 본문의 데이터는 util.IOUtils 클래스의 readData() 메소드를 활용한다
+* 본문의 길이는 HTTP 헤더의 Content-Length의 값이다
+</details>
 
 ### 요구사항 4 - redirect 방식으로 이동
-* 
+<details> <summary><b>Hint</b></summary>
 
-### 요구사항 5 - cookie
-* 
+* 302 status code를 찾아본다
 
-### 요구사항 6 - stylesheet 적용
-* 
+</details>
 
-### heroku 서버에 배포 후
-* 
+### 요구사항 5 - 로그인 하기
+#### /user/login.html으로 이동해 로그인을 할 수 있다
+#### 로그인 성공 시 Set-Cookie를 추가해 로그인 성공 여부를 전달하고 /login.html로 이동한다
+#### 실패 시 로그인 실패 여부를 전달하고 /user/login_failed.html로 이동한다
+* ex) `Cookie: logined=true` or `Cookie: logined=false`
+
+### 요구사항 6 - 사용자 목록 출력
+로그인에 성공한 사람의 목록을 페이지로 만들어서 반환한다
+<details> <summary><b>Hint</b></summary>
+
+* 로그인 여부를 판단하기 위해 Cookie값을 확인한다
+* util.HttpRequestUtils 클래스의 parseCookies 메소드를 활용하여 Cookie를 파싱한다
+* StringBuilder을 활용하여 HTML을 동적으로 생성한 후 응답을 보낸다
+</details>
+
+### 요구사항 7 - stylesheet 적용
+* 확장자가 css, js로 들어오는 요청을 처리한다
+<details> <summary><b>Hint</b></summary>
+
+* 응답 헤더의 Content-Type을 변경한다
+* css: `text/css`, js: `application/javascript`
+</details>
+</details>
+
+
--- src/web-application-server/src/main/java/webserver/RequestHandler.java
+++ src/web-application-server/src/main/java/webserver/RequestHandler.java
@@ -2,7 +2,9 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.Map;
 
 import db.DataBase;
@@ -22,9 +24,27 @@ public class RequestHandler extends Thread {
 
     private final Socket connection;
 
+    private String httpMethod = "";
+
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
+
+    public void run() {
+        /*
+          InputStream: 클라이언트(브라우저)에서 서버로 요청을 보낼 때 전달되는 데이터 스트림
+          OutputStream: 서버에서 클라이언트에 응답을 보낼 때 전달되는 데이터를 담당하는 스트림
+         */
+        try (InputStream in = connection.getInputStream();
+             OutputStream out = connection.getOutputStream()) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String path = getPath(br);
+            responseByRequestToPath(path, br);
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
 
     private String getContextType(String path){
         if (path == null) return  null;
@@ -37,8 +57,8 @@ public class RequestHandler extends Thread {
         return path.startsWith("/user/create");
     }
 
-    private void createUser(String bodyData) {
-//      NOTE: 정상적인 쿼리스트링이 아닌경우
+    private void postCreateUser(String bodyData) {
+        // NOTE: 정상적인 쿼리스트링이 아닌경우
         if (bodyData == null)   return;
         Map<String, String> user = HttpRequestUtils.parseQueryString(bodyData);
         User tmp = new User(user.get("userId"), user.get("password"), user.get("name"), user.get("email"));
@@ -46,7 +66,6 @@ public class RequestHandler extends Thread {
     }
 
     private boolean checkUser(String bodyData){
-//        userId=a&password=a
         if (bodyData == null) return false;
         Map<String, String> user = HttpRequestUtils.parseQueryString(bodyData);
         User find = DataBase.findUserById(user.get("userId"));
@@ -66,56 +85,117 @@ public class RequestHandler extends Thread {
         return IOUtils.readData(br, contentLength);
     }
 
-    public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
 
-        /*
-          InputStream: 클라이언트(브라우저)에서 서버로 요청을 보낼 때 전달되는 데이터 스트림
-          OutputStream: 서버에서 클라이언트에 응답을 보낼 때 전달되는 데이터를 담당하는 스트림
-         */
-        String path;
-        String contentType;
-        try (InputStream in = connection.getInputStream();
-             OutputStream out = connection.getOutputStream();
-             DataOutputStream dos = new DataOutputStream(out);
-             BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
-            path = getPath(br);
-            contentType = getContextType(path);
-//            회원가입일 경우
-            if (isCreateUser(path)){
-                createUser(getRequestBody(br));
-                response302Header(dos);
-                return;
-            }
-            if (isLogin(path)){
-                boolean flag = checkUser(getRequestBody(br));
-                try {
-                    dos.writeBytes("HTTP/1.1 302 Found \r\n");
-                    dos.writeBytes("Content-Type: text-html/\r\n"); // 변경된 부분
-                    dos.writeBytes("Location: " + (flag ? "/index.html" : "/user/login_failed.html") + "\r\n");
-                    dos.writeBytes("Set-Cookie: logined="+ (flag ? "true" : "false") + "; Path=/\r\n");
-                    dos.writeBytes("\r\n");
-                } catch (IOException e) {
-                    log.error(e.getMessage());
-                }
-                return;
-            }
-
-
-            try {
-                byte[] body = getBody(path);
-                response200Header(dos, body.length, contentType);
-                responseBody(dos, body);
-            }catch (IOException e){
-                badRequest();
-            }
-
-
-
-        } catch (IOException e) {
-            log.error(e.getMessage());
+    private void responseByRequestToPath(String path, BufferedReader br) throws IOException{
+        //NOTE: 회원 가입
+        if (isCreateUser(path)){
+            responseCreateUser(br, path);
+            return;
         }
+        //NOTE: 로그인
+        if (isLogin(path)){
+            responseLogin(checkUser(getRequestBody(br)));
+            return;
+        }
+        //NOTE: 사용자 목록
+        if(isUserList(path)){
+            responseUserList(br);
+            return;
+        }
+        otherRequest(path);
+    }
+
+    private void otherRequest(String path) throws IOException {
+        try {
+            byte[] body = getBody(path);
+            response200Header(body.length, getContextType(path));
+            responseBody(body);
+        }catch (IOException e){
+            responseNotFound();
+        }
+    }
+
+    private void responseUserList(BufferedReader br) throws IOException {
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        if (!isConnectUser(br)){
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Content-Type: text/html \r\n");
+            dos.writeBytes("Location: " + "/user/login.html" + "\r\n");
+            dos.writeBytes("\r\n");
+            return;
+        }
+        connectUserListPage();
+    }
+
+    //NOTE: webapp/user/login.html 에서 form method 확인
+    private void responseCreateUser(BufferedReader br, String path) throws  IOException{
+        if ("GET".equals(httpMethod)) getCreateUser(path);
+        if ("POST".equals(httpMethod)) postCreateUser(getRequestBody(br));
+        response302Header();
+    }
+
+    private void getCreateUser(String path) {
+        Map<String, String> user = HttpRequestUtils.parseQueryString(path.substring(path.indexOf("?") + 1));
+        User tmp = new User(user.get("userId"), user.get("password"), user.get("name"), user.get("email"));
+        DataBase.addUser(tmp);
+    }
+
+    private void responseLogin(boolean flag) throws IOException {
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Content-Type: text/html \r\n");
+        dos.writeBytes("Location: " + (flag ? "/index.html" : "/user/login_failed.html") + "\r\n");
+        dos.writeBytes("Set-Cookie: logined="+ (flag ? "true" : "false") + "; Path=/\r\n");
+        dos.writeBytes("\r\n");
+    }
+
+    private String userListPage(){
+        StringBuilder htmlBuilder = new StringBuilder();
+        htmlBuilder.append("<html>\n");
+        htmlBuilder.append("<title>Hello World</title>\n");
+        htmlBuilder.append("<body>\n");
+        htmlBuilder.append("<h1>접속중인 유저 목록</h1>\n");
+        Collection<User> users = DataBase.findAll();
+        for (User user: users){
+            htmlBuilder.append("<span>");
+            htmlBuilder.append(user.getUserId());
+            htmlBuilder.append("</span>\n");
+        }
+        htmlBuilder.append("</body>\n");
+        htmlBuilder.append("</html>\n");
+        return  htmlBuilder.toString();
+    }
+    private void connectUserListPage() throws IOException {
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        String htmlBuilder = userListPage();
+
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: text/html; charset=utf-8\r\n");
+        dos.writeBytes("Content-Length: " + htmlBuilder.length() + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(htmlBuilder.getBytes(StandardCharsets.UTF_8), 0, htmlBuilder.length());
+        dos.flush();
+    }
+
+    private boolean isConnectUser(BufferedReader br) throws IOException {
+        String line = br.readLine();
+        String separator = "Cookie: ";
+        String getCookieKey = "logined";
+        while (!"".equals(line)) {
+            if (line.startsWith(separator)){
+                try {
+                    return Boolean.parseBoolean((HttpRequestUtils.parseCookies(line.substring(separator.length()))).get(getCookieKey));
+                }catch (NullPointerException e){
+                    return  false;
+                }
+            }
+            line = br.readLine();
+        }
+        return  false;
+    }
+
+    private boolean isUserList(String path) {
+        return path.startsWith("/user/list");
     }
 
 
@@ -123,55 +203,43 @@ public class RequestHandler extends Thread {
         return Files.readAllBytes(new File("src/web-application-server/webapp" + path).toPath());
     }
 
-    private void badRequest(){
-        try {
-//            TODO: 할당 실패함
-            DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
-            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
-            dos.writeBytes("Content-Type: text/html\r\n"); // 변경된 부분
-            byte[] body = Files.readAllBytes(new File("src/web-application-server/webapp/notFound.html").toPath());
-            dos.writeBytes("Content-Length: " + body.length + "\r\n");
-            dos.writeBytes("\r\n");
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private void responseNotFound() throws IOException{
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
+        dos.writeBytes("Content-Type: text/html\r\n"); // 변경된 부분
+        byte[] body = Files.readAllBytes(new File("src/web-application-server/webapp/notFound.html").toPath());
+        dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        dos.writeBytes("\r\n");
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 
     private String getPath(BufferedReader br) throws  IOException{
         String line = br.readLine();
         if (line == null) return "/notFound.html";
-        String path = line.split(" ").length > 1 ? line.split(" ")[1] : null;
-        if ("/".equals(path)) path = "/index.html";
-        return path;
+        String[] split = line.split(" ");
+        if (split.length < 3) return "/notFound.html";
+        httpMethod = split[0];
+        if ("/".equals(split[1])) return "/index.html";
+        return split[1];
     }
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent, String contentType) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: " + contentType + "\r\n"); // 변경된 부분
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private void response200Header(int lengthOfBodyContent, String contentType) throws IOException {
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.writeBytes("HTTP/1.1 200 OK \r\n");
+        dos.writeBytes("Content-Type: " + contentType + "\r\n"); // 변경된 부분
+        dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+        dos.writeBytes("\r\n");
     }
-    private void response302Header(DataOutputStream dos) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: " + "/index.html" + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private void response302Header() throws IOException {
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.writeBytes("HTTP/1.1 302 Found \r\n");
+        dos.writeBytes("Location: " + "/index.html" + "\r\n");
+        dos.writeBytes("\r\n");
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+    private void responseBody(byte[] body) throws IOException{
+        DataOutputStream dos = new DataOutputStream(connection.getOutputStream());
+        dos.write(body, 0, body.length);
+        dos.flush();
     }
 }
