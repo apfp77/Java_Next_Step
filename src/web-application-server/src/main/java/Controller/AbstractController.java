32ecb149359cab97de160dfe737b4a91eff6823e
apfp77 <apfp77@naver.com>
2024-03-13 09:09:49 +0900

    feat: Controller 템플릿 추가
    - Controller interface 추가
    - 추상클래스 AbstractController 추가

---
+++ src/web-application-server/src/main/java/Controller/AbstractController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+import http.HttpRequest;
+import http.HttpResponse;
+
+public abstract class AbstractController implements Controller{
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) {}
+    public void doPost(HttpRequest request, HttpResponse response){}
+    public void doGet(HttpRequest request, HttpResponse response){}
+
+}
---
+++ src/web-application-server/src/main/java/Controller/Controller.java
@@ -0,0 +1,8 @@
+package Controller;
+
+import http.HttpRequest;
+import http.HttpResponse;
+
+public interface Controller {
+    void service(HttpRequest request, HttpResponse response);
+}
